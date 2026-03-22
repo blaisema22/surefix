72ea32432ffff255a5ec0da3104c50bc61171bdc
blaisema22 <manishimweblaise603@gmail.com>
2026-03-22 03:28:04 -0700

    chore: clean up repository and apply improved .gitignore

--- auth.utils.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-function generateToken(payload) {
-    const jwtSecret = process.env.JWT_SECRET || 'surefix_secret_key_change_in_production';
-    return jwt.sign(payload, jwtSecret, { expiresIn: '7d' });
-}
-module.exports = { generateToken };
